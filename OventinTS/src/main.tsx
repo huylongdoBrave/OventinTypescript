31646bd3ec81793438f81736392f270d897e6dc7
huylongdoBrave <huylong.do@estuary.solutions>
2025-12-10 19:29:14 +0700

    fix: change link

--- OventinTS/src/main.tsx
+++ OventinTS/src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import {  HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter>
     <App />
-    </BrowserRouter>
+    </BrowserRouter> */}
+
+    <HashRouter>
+      <App />
+    </HashRouter>
+
   </StrictMode>,
 )
