9233084281cf6c5e2c01ee62f6784f0f2565038f
huylongdoBrave <huylong.do@estuary.solutions>
2025-12-17 10:21:47 +0700

    style: change css form infoProfile

--- OventinTS/src/components/Profile/InfoProfile.tsx
+++ OventinTS/src/components/Profile/InfoProfile.tsx
@@ -23,9 +23,9 @@ const InfoProfile: React.FC = () => {
   // const location = useLocation();
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
+  const [activeTab, setActiveTab] = useState<'personal' | 'recipient'>('personal');
   // const [currentUser, setCurrentUser] = useState<StoredUser | null>();
-  const [currentUser, setCurrentUser] = useState<StoredUser | null>(() => {
-    // Khởi tạo state từ localStorage
+  const [currentUser, setCurrentUser] = useState<StoredUser | null>(() => {     // Khởi tạo state từ localStorage
     const storedUser = localStorage.getItem('loggedInUser');
     return storedUser ? JSON.parse(storedUser) as StoredUser : null;
   });
@@ -62,28 +62,29 @@ const InfoProfile: React.FC = () => {
           // Gọi API
           const response = await getApiInfoProfile();           // Gọi Api
           console.log("DATA PROFILE: ", response.data);         // Kiểm tra data trả về từ API
-          const UserData = response.data  // Lấy loại data json từ response
+          const UserData = response.data                        // Lấy loại data json từ response
 
-          const StoredUser: StoredUser = {
+          const NewStoredUser: StoredUser = {
             fullName: UserData.data.fullName || UserData.data.full_name || UserData.data.name || "Chưa cập nhật",
             phoneNumber: UserData.data.phoneNumber || UserData.data.phone || "Chưa cập nhật",
             email: UserData.data.email || "Chưa cập nhật",
             dateOfBirth: UserData.data.birthday || UserData.data.dateOfBirth || "",
           };
 
-          // Cập nhật State để màn hình hiển thị dữ liệu mới 
-          setCurrentUser(StoredUser);
-
-          // Cập nhật ngược lại vào LocalStorage để lần sau vào load nhanh
+          // Cập nhật State với dữ liệu mới 
+          setCurrentUser(NewStoredUser);
+          // Cập nhật lại LocalStorage với dữ liệu mới
           localStorage.setItem('loggedInUser', JSON.stringify(UserData));
+
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
         } catch (error: any) {
           console.error("Lỗi lấy thông tin mới (đang hiển thị thông tin cũ):", error);
-        if (error.response && error.response.status === 401) {
-            alert("Phiên đăng nhập hết hạn. Vui lòng đăng nhập lại.");
-            // localStorage.removeItem('token'); // Xóa token hư
-            navigate('/'); // Đá về trang chủ
-          }
+          if (error.response && error.response.status === 401) {
+              alert("Phiên đăng nhập hết hạn. Vui lòng đăng nhập lại.");
+              localStorage.removeItem('token'); // Xóa token cũ
+              localStorage.removeItem('loggedInUser'); // Xóa thông tin người dùng cũ
+              navigate('/'); // về trang chủ
+            }
         } finally {
           setIsLoading(false);
         }
@@ -166,19 +167,88 @@ const InfoProfile: React.FC = () => {
         description={alertState.description}
         onClose={() => setAlertState({ ...alertState, isOpen: false })}
       />
-      <div className="p-6 max-w-lg mx-auto bg-[url('/static/modal.png')] 
-                        bg-cover bg-center rounded-[20px] border-4 border-white mt-10 mb-10">
-        <h2 className="text-2xl font-bold text-center text-white mb-6">Thông tin tài khoản</h2>
-        {isLoading && <p>Đang đồng bộ dữ liệu...</p>} 
-        {/* Container for user info */}
-        <div className="flex flex-col gap-3 text-[#233da3]">
-          {renderInput("fullName", "Họ và tên", currentUser.fullName, true)}
-          {renderInput("phoneNumber", "Số điện thoại", currentUser.phoneNumber, true)}
-          {renderInput("birthday", "Ngày sinh", currentUser.dateOfBirth, true)}
-          {renderInput("city", "Thành phố", "HCM", true)}
-          {renderInput("email", "Email", currentUser.email || 'Chưa cập nhật', true)}
+
+      <div className="flex justify-center items-center  w-full h-full relative z-0">
+        <img
+          src="/static/InfoSelf.png"
+          alt="Lucky Draw"
+          className="h-auto w-[70%] max-w-[300px] md:w-[80%] md:max-w-[400px] lg:w-[90%]"
+        />
+      </div>
+
+      {/* Tab container */}
+      <div className="flex justify-center items-start gap-2 sm:gap-4 mt-4 mb-8 px-2">
+        {/* Tab 1 Wrapper */}
+        <div className="relative">
+          <div
+            onClick={() => setActiveTab('personal')}
+            className={`
+            w-[160px] md:w-[250px] lg:w-[250px] py-3 md:py-4 text-white -skew-x-[10deg] relative 
+            font-bold uppercase tracking-wider
+            drop-shadow-[8px_8px_0px_rgba(0,0,0,0.3)]
+            [clip-path:polygon(0_10,_100%_0,_calc(100%-15px)_100%,_-15px_100%)]
+            transition-all duration-200
+            hover:scale-105 hover:-translate-y-1
+            active:translate-x-[2px] active:translate-y-[2px]
+            active:drop-shadow-[4px_4px_0px_rgba(0,0,0,0.3)]
+            focus:outline-none cursor-pointer
+            ${activeTab === 'personal' ? 'bg-blue-700' : 'bg-gray-600 hover:bg-gray-700'}`}
+          >
+            <span className="block skew-x-[10deg] text-center">
+            Tài khoản cá nhân
+            </span>
+          </div>
+          {/* Đường kẻ cho Tab 1 */}
+          <div className={`absolute w-43 md:w-66 lg:w-66 bottom-[-20px] left-[-10px] right-[10px] h-1 transition-colors duration-300 ${activeTab === 'personal' ? 'bg-blue-500 z-10' : 'bg-white/50'}`}></div>
+        </div>
+
+        {/* Tab 2 Wrapper */}
+        <div className="relative">
+          <div
+            onClick={() => setActiveTab('recipient')}
+            className={`
+            w-[160px] md:w-[250px] lg:w-[250px] py-3 md:py-4 text-white -skew-x-[10deg] relative 
+            font-bold uppercase tracking-wider
+            drop-shadow-[8px_8px_0px_rgba(0,0,0,0.3)]
+            [clip-path:polygon(0_10,_100%_0,_calc(100%-15px)_100%,_-15px_100%)]
+            transition-all duration-200
+            hover:scale-105 hover:-translate-y-1
+            active:translate-x-[2px] active:translate-y-[2px]
+            active:drop-shadow-[4px_4px_0px_rgba(0,0,0,0.3)]
+            focus:outline-none cursor-pointer
+            ${activeTab === 'recipient' ? 'bg-blue-700' : 'bg-gray-600 hover:bg-gray-700'}`}
+          >
+            <span className="block skew-x-[10deg] text-center">
+            Thông tin người nhận
+            </span>
+          </div>
+          {/* Đường kẻ cho Tab 2 */}
+          <div className={`absolute w-43 md:w-66 lg:w-66 bottom-[-20px] left-[-10px] right-[10px] h-1 transition-colors duration-300 ${activeTab === 'recipient' ? 'bg-blue-500 z-10' : 'bg-white/50'}`}></div>
         </div>
       </div>
+
+      {/* Tab content */}
+      {activeTab === 'personal' && (
+        <div className="p-4 sm:p-6 max-w-lg w-sm lg:w-lg md:w-md mx-auto bg-[url('/static/modal.png')] 
+                          bg-center rounded-[20px] border-4 border-white mt-10 mb-10">
+          <h2 className="text-2xl font-bold text-center text-white mb-6">Thông tin tài khoản</h2>
+          {isLoading && <p>Đang đồng bộ dữ liệu...</p>} 
+          {/* Container for user info */}
+          <div className="flex flex-col gap-3 text-[#233da3]">
+            {renderInput("fullName", "Họ và tên", currentUser.fullName, true)}
+            {renderInput("phoneNumber", "Số điện thoại", currentUser.phoneNumber, true)}
+            {renderInput("birthday", "Ngày sinh", currentUser.dateOfBirth, true)}
+            {renderInput("city", "Thành phố", "HCM", true)}
+            {renderInput("email", "Email", currentUser.email || 'Chưa cập nhật', true)}
+          </div>
+        </div>
+      )}
+
+      {activeTab === 'recipient' && (
+        <div className="p-6 max-w-lg mx-auto mt-10 mb-10 text-white text-center">
+          <p>Chức năng đang được phát triển.</p>
+        </div>
+      )}
 
     </>
   );
